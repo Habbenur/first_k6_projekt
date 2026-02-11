8ade11740fcd13bbff1fd5a4a654c2e10c6478a6
Habbenur <habbenurozen@gmail.com>
2026-02-11 10:11:45 +0100

    delete to unwanted files

--- k6-frans-api/my-first-test.js
+++
@@ -1,5 +0,0 @@
-// Import the http module to make HTTP requests. From this point, you can use `http` methods to make HTTP requests.
-import http from 'k6/http';
-
-// Import the sleep function to introduce delays. From this point, you can use the `sleep` function to introduce delays in your test script.
-import { sleep } from 'k6';
